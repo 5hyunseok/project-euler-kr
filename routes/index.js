a90e94e3e7ba08af2dc676d60b2dacae80875b8c
O <5hyunseok@O.local>
2018-02-19 13:48:15 +0900

    Modify main to about

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var defaultTitle = '프로젝트 오일러 - 한글';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('about', { title: defaultTitle });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About'});
+  res.render('about', { title: defaultTitle });
 })
 
 module.exports = router;
