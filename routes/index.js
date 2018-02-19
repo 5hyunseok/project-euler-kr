461423e3dbc7f893ff6b49268f1543bc509333cf
O <5hyunseok@O.local>
2018-02-19 14:49:07 +0900

    Add Archives page

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,43 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: defaultTitle });
-})
+  res.render('about', { title: defaultTitle, about_id: 'current' });
+});
+
+router.get('/archives', function(req, res, next) {
+  res.redirect('/archives/page/1');
+  // res.render('problem', { title: defaultTitle, archives_id: 'current' });
+});
+
+router.get('/archives/page/:number', function(req, res, next) {
+  var pageNum = req.params.number;
+  var totalNum = 610;
+  var totalPageNum = Math.ceil(totalNum / 50);
+  var isNum = /^\d+$/.test(pageNum);
+
+  // check valid page number
+  if (pageNum > totalPageNum || pageNum < 1 || !isNum) {
+    res.status(500);
+    res.render('error', { message: '유효하지 않은 페이지입니다.' });
+  }
+
+  var pages = [];
+  for(var i = 1; i <= totalPageNum; i++){
+    var page = { num: i };
+    if (pageNum == i) {
+      page.current = true;
+    } else {
+      page.current = false;
+    }
+    pages.push(page);
+  }
+  res.render('problem', {
+    title: defaultTitle,
+    archives_id: 'current',
+    total_num: totalNum,
+    total_page_num: totalPageNum,
+    pages: pages
+  });
+});
 
 module.exports = router;
