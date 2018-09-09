6343134fb191a54b743bca6d82b2d6c389bce4a1
5hyunseok <5xhyunseok@gmail.com>
2018-09-09 17:11:57 +0900

    Modify archive table for link to forum

--- src/components/ArchiveTable.vue
+++ src/components/ArchiveTable.vue
@@ -15,7 +15,25 @@
         <td>
           <div style="text-align:center;">{{ problem.correctCount }}</div>
         </td>
-        <td><img :src=solved></td>
+        <td>
+          <table style="width:100%;" class="no_border">
+            <tbody>
+              <tr>
+                <td style="width:20px;">
+                  <div style="text-align:center;"><img :src=solved alt="Solved" title="Solved"><br></div>
+                </td>
+                <!-- <td style="width:20px;"><a href="overview=001" title="Download overview PDF"><img src="images/icon_pdf.png" alt=""></a></td> -->
+                <td style="width:20px;">
+                  <a href=""><img :src=forum alt=""></a>
+                </td>
+                <td>
+                  <div style="font-size:80%;text-align:right;color:#999;">{{ problem.postCount }} posts</div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <!-- <img :src=solved> <img :src=forum> <div style="font-size:80%;text-align:right;color:#999;">{{ problem.postCount }} posts</div></td> -->
+        </td>
       </tr>
     </tbody>
   </table>
@@ -23,6 +41,7 @@
 
 <script>
 import solved from '@/assets/solved.png';
+import forum from '@/assets/forum.png';
 
 export default {
   name: 'ArchiveTable',
@@ -30,6 +49,7 @@ export default {
   data() {
     return {
       solved,
+      forum,
     };
   },
 };
