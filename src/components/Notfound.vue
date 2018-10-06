1409baeb987bdf8d9f5d1022856a23de9b0ef557
5hyunseok <5xhyunseok@gmail.com>
2018-10-06 17:37:49 +0900

    Add 404 page

---
+++ src/components/Notfound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="error_page">
+    <img :src=errorImage alt="page not found">
+    <h3>존재하지 않는 페이지입니다.</h3>
+  </div>
+
+</template>
+
+<script>
+import errorImage from '@/assets/error_page.png';
+
+export default {
+  name: 'About',
+  data() {
+    return {
+      errorImage,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
