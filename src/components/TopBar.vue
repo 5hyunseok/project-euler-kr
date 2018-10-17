2fc0b5822e46e685be720ab09aea0af8b7624fff
5hyunseok <5xhyunseok@gmail.com>
2018-10-17 14:19:06 +0900

    Add current style

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -3,14 +3,14 @@
     <div id="header" class="noprint">
       <div id="nav" class="noprint">
         <ul>
-          <li><router-link :to="{ name: 'about', params: {} }">소개</router-link></li>
-          <li><router-link :to="{ name: 'archives', params: { pageNumber: 1 } }">문제</router-link></li>
+          <li v-bind:id="routeName == 'about' ? 'current' : ''"><router-link :to="{ name: 'about', params: {} }">소개</router-link></li>
+          <li v-bind:id="routeName == 'archives' ? 'current' : ''"><router-link :to="{ name: 'archives', params: { pageNumber: 1 } }">문제</router-link></li>
 
-          <li><router-link :to="{ name: 'recent' }">최근 문제</router-link></li>
-          <li v-if="token"><router-link :to="{ name: 'mypage' }">내 계정</router-link></li>
-          <li><router-link :to="{ name: 'news' }">뉴스</router-link></li>
-          <li v-if="!token"><router-link :to="{ name: 'register' }">회원 가입</router-link></li>
-          <li v-if="!token"><router-link :to="{ name: 'login' }">로그인</router-link></li>
+          <li v-bind:id="routeName == 'recent' ? 'current' : ''"><router-link :to="{ name: 'recent' }">최근 문제</router-link></li>
+          <li v-if="token" v-bind:id="routeName == 'mypage' ? 'current' : ''"><router-link :to="{ name: 'mypage' }">내 계정</router-link></li>
+          <li v-bind:id="routeName == 'news' ? 'current' : ''"><router-link :to="{ name: 'news' }">뉴스</router-link></li>
+          <li v-if="!token" v-bind:id="routeName == 'register' ? 'current' : ''"><router-link :to="{ name: 'register' }">회원 가입</router-link></li>
+          <li v-if="!token" v-bind:id="routeName == 'login' ? 'current' : ''"><router-link :to="{ name: 'login' }">로그인</router-link></li>
           <li v-if="token"><a href="#logout" v-on:click="logout">로그아웃</a></li>
         </ul>
       </div>
@@ -35,6 +35,7 @@ export default {
     return {
       msg: 'Hi',
       testToken: '',
+      routeName: '',
     };
   },
   computed: {
@@ -49,6 +50,11 @@ export default {
     logout() {
       this.$store.commit('users/resetToken');
       this.$router.push({ name: 'about' });
+    },
+  },
+  watch: {
+    '$route' (to, from) {
+      this.routeName = to.name;
     },
   },
 };
